c7e8c80a89c9368b0df7b30495f48dd204de88dc
ssbon-renewal <renewal@local>
2026-08-04 07:03:38 +0000

    틀 고정 CSS 실제 적용 (직전 커밋 누락분)

--- js/config.js
+++ js/config.js
@@ -5,5 +5,5 @@ window.SSBON = window.SSBON || {};
 window.SSBON.config = {
   restUrl: 'https://jazyhsyylqrmvnazsgqt.supabase.co/rest/v1',
   anonKey: 'sb_publishable_d5Ehy03ghIozA7RrjFsa5g_rAaHaPyD',
-  version: 'v0.9.11-shell'
+  version: 'v0.9.12-shell'
 };
